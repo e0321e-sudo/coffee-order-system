3adace68ea3fd1812e158480cff028b141881d13
t2025-m0040 <e0321e@nate.com>
2026-04-30 19:36:23 +0900

    feat: initialize Spring Boot project

---
+++ src/test/java/com/sparta/coffee/CoffeeOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeOrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
